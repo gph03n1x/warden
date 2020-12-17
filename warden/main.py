87b550014592887f42c0b9555e7e88148b4aec85
Giannis Papaioannou <giannispapcod7@gmail.com>
2020-12-17 15:24:48 +0200

    fix: Handles network errors

--- warden/main.py
+++ warden/main.py
@@ -18,7 +18,14 @@ class IfConfig:
 
 def trigger_notification(ip_address):
     message = {"content": f"Current minecraft IP address {ip_address}"}
-    requests.post(WEBHOOK, data=message)
+
+    try:
+        requests.post(WEBHOOK, data=message)
+    except (requests.ConnectionError, requests.Timeout, requests.exceptions.HTTPError) as exc:
+        Logger.info(f"Failed to notify with IP address: {exc}")
+        return False
+    else:
+        return True
 
 
 def monitor():
@@ -26,18 +33,23 @@ def monitor():
     service = IfConfig()
     ip_address = service.get_ip()
     Logger.info(f"Starting IP address {ip_address}")
+    trigger_notification(ip_address)
     while True:
         time.sleep(INTERVAL)
+        try:
+            current_ip_address = service.get_ip()
+        except (requests.ConnectionError, requests.Timeout, requests.exceptions.HTTPError) as exc:
+            Logger.info(f"Failed to fetch IP address: {exc}")
+        else:
+            Logger.debug(f"Fetched IP {current_ip_address}")
+            if current_ip_address != ip_address:
+                Logger.info(
+                    "Detected IP change."
+                    f"Current IP {current_ip_address}, Previous IP {ip_address}"
+                )
 
-        current_ip_address = service.get_ip()
-        Logger.debug(f"Fetched IP {current_ip_address}")
-        if current_ip_address != ip_address:
-            Logger.info(
-                "Detected IP change."
-                f"Current IP {current_ip_address}, Previous IP {ip_address}"
-            )
-            ip_address = current_ip_address
-            trigger_notification(ip_address)
+                if trigger_notification(current_ip_address):
+                    ip_address = current_ip_address
 
 
 if __name__ == "__main__":
